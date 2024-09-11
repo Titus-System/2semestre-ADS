f480923517d051d65b9a1539ddc324c4b94701ae
pedro-fs-garcia <pedrofsgarcia.pro@gmail.com>
2024-09-10 22:11:40 -0300

    conexão ollama

--- src/main/java/com/titus_systems/idscan/Main.java
+++ src/main/java/com/titus_systems/idscan/Main.java
@@ -1,7 +1,11 @@
 package com.titus_systems.idscan;
 
+import com.titus_systems.ollama.OllamaConnection;
+
+
 public class Main {
     public static void main(String[] args) throws Exception{
         System.out.println("Hello World!");
     }
+
 }
---
+++ src/main/java/com/titus_systems/ollama/OllamaConnection.java
@@ -0,0 +1,18 @@
+package com.titus_systems.ollama;
+
+import io.github.ollama4j.OllamaAPI;
+
+public class OllamaConnection{
+
+    private OllamaAPI ollamaAPI;
+
+    public OllamaConnection(){
+        String host = "http://localhost:11434/";
+        OllamaAPI ollamaAPI = new OllamaAPI(host);
+    }
+
+    public void chageHost(String host){
+        this.ollamaAPI = new OllamaAPI(host);
+    }
+
+}
