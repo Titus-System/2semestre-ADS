475ff7d983924b342713b172585d4083a8e7559d
Tiny-Mushroom <aliceazambujaalves@gmail.com>
2024-09-13 09:50:32 -0300

    fixed import

--- src/main/java/com/titus_systems/idscan/Main.java
+++ src/main/java/com/titus_systems/idscan/Main.java
@@ -1,7 +1,6 @@
 package com.titus_systems.idscan;
 
-import com.titus_systems.ollama.OllamaConnection;
-
+import com.titus_systems.idscan.ollama.OllamaConnection;
 
 public class Main {
     public static void main(String[] args) throws Exception{
