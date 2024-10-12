ae655067b57dc218852402235891fcdc1864a777
juliasoares17 <juliapereira1448@gmail.com>
2024-10-12 11:14:17 -0300

    style(gui): Adição de detalhes na tela de confirmação

--- src/main/java/com/titus_systems/idscan/gui/ConfirmaçãoController.java
+++ src/main/java/com/titus_systems/idscan/gui/ConfirmaçãoController.java
@@ -1,5 +1,23 @@
 package com.titus_systems.idscan.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class ConfirmaçãoController {
     
+    @FXML
+    private ImageView exibiçãoImagem;
+
+    @FXML
+    private Button cancelar;
+
+    @FXML
+    private Button confirmar;
+
+    public void setImage(Image image) {
+        exibiçãoImagem.setImage(image);
+    }
+
 }
