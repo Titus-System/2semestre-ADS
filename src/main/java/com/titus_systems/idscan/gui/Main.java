de26110e8a59bb773bc722d5b5b552c121330865
pedro garcia <pedrofsgarcia.pro@gmail.com>
2024-11-18 21:21:01 -0300

    fix(Main): verifica se Ollama está rodando

--- src/main/java/com/titus_systems/idscan/gui/Main.java
+++ src/main/java/com/titus_systems/idscan/gui/Main.java
@@ -1,5 +1,9 @@
 package com.titus_systems.idscan.gui;
 
+import javax.swing.JOptionPane;
+
+import com.titus_systems.idscan.ollama.OllamaEngine;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -14,6 +18,12 @@ public class Main extends Application {
         primaryStage.setTitle("ID Scanner");
         primaryStage.setScene(new Scene(root, 1150, 750)); // Ajuste o tamanho conforme necessário
         primaryStage.show();
+        OllamaEngine ollamaEng = new OllamaEngine("gemma2:2b", 0.8f);
+        try {
+            ollamaEng.ping();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Ollama não está em execução.\nInicie o Ollama para que a aplicação funcione corretamente.");
+        }
     }
 
     public static void main(String[] args) {
